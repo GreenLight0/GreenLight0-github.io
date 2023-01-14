fda21c69efd92ac7d79f61ef768d3a9ec8f29268
GreenLight0 <122675952+GreenLight0@users.noreply.github.com>
2023-01-14 12:53:13 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+include("connexion.php");
+try {
+    $bd = new PDO ( "mysql:host = $server; dbname=$base", "$user", "$password"); 
+    $bd->exec("SET NAMES utf8");
+}
+catch (Exception $e){
+    print_r($e -> getMessage());
+    die("Erreur de connexion à la base ");
+}
+?>
